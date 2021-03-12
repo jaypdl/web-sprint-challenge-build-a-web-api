d8f5b00e7531b23cde24e5c12b8ada3c7c7e91da
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-03-12 09:05:51 -0700

    wip

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+const server = require('./api/server')
+
+const PORT = process.env.PORT || 4000
+
+server.listen(PORT, () => {
+  console.log(`\n***** The Server Is JUDGING YOU on Port ${PORT} 😲 *****\n`)
+})
