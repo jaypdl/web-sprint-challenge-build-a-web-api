bf66146fab3c56820feaf33d356c7c592b8f6736
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-03-12 09:14:18 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
-const express = require('express');
-const server = express();
-
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
+const express = require('express');
+const actionsRouter = require('./actions/actions-router')
+const projectsRouter = require('./projects/projects-router')
+
+
+const server = express();
+server.use(express.json())
+
+// Routes
+server.use('/api/actions', actionsRouter)
+server.use('/api/project', projectsRouter)
 
 module.exports = server;
